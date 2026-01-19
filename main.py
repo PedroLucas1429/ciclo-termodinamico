5de7bb67efffa601e034feb000adb0cfacc5f582
Pedro Lucas <plucas14252931@gmail.com>
2026-01-19 09:56:07 -0300

    modificando host

--- main.py
+++ main.py
@@ -168,4 +168,4 @@ def processar_circuito():
         return jsonify({"erro": "Falha ao gerar PDF.", "detalhes": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
